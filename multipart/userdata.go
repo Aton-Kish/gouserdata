901cf18ae419e19bf1b506cb84c99fb98b5423b9
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-07-23 12:48:30 +0000

    add userdata

---
+++ multipart/userdata.go
@@ -0,0 +1,101 @@
+// Copyright (c) 2022 Aton-Kish
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package multipart
+
+import (
+	"bytes"
+	"fmt"
+	"mime"
+)
+
+const (
+	defaultBoundary    = "+Go+User+Data+Boundary=="
+	defaultMIMEVersion = "1.0"
+)
+
+type UserData struct {
+	Header   Header
+	Parts    []Part
+	boundary string
+}
+
+func NewUserData() *UserData {
+	return NewUserDataWithBoundary(defaultBoundary)
+}
+
+func NewUserDataWithBoundary(boundary string) *UserData {
+	typ := mime.FormatMediaType("multipart/mixed", map[string]string{"boundary": boundary})
+
+	h := NewHeader()
+	h.Set("Content-Type", typ)
+	h.Set("Mime-Version", defaultMIMEVersion)
+
+	p := make([]Part, 0)
+
+	return &UserData{Header: *h, Parts: p, boundary: boundary}
+}
+
+func (d *UserData) AddPart(mediaType string, body []byte) {
+	part := NewPart(mediaType, body)
+	d.Parts = append(d.Parts, *part)
+}
+
+func (d *UserData) Render() ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	h, err := d.Header.Render()
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := buf.Write(h); err != nil {
+		return nil, err
+	}
+
+	if _, err := buf.WriteString("\r\n"); err != nil {
+		return nil, err
+	}
+
+	for _, part := range d.Parts {
+		if _, err := buf.WriteString(fmt.Sprintf("--%s\r\n", d.boundary)); err != nil {
+			return nil, err
+		}
+
+		p, err := part.Render()
+		if err != nil {
+			return nil, err
+		}
+
+		if _, err := buf.Write(p); err != nil {
+			return nil, err
+		}
+
+		if _, err := buf.WriteString("\r\n"); err != nil {
+			return nil, err
+		}
+	}
+
+	if _, err := buf.WriteString(fmt.Sprintf("--%s--\r\n", d.boundary)); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
---
+++ multipart/userdata_test.go
@@ -0,0 +1,322 @@
+// Copyright (c) 2022 Aton-Kish
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package multipart
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUserData(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected *UserData
+	}{
+		{
+			name: "positive case",
+			expected: &UserData{
+				Header: func() Header {
+					h := NewHeader()
+
+					h.Set("Content-Type", `multipart/mixed; boundary="+Go+User+Data+Boundary=="`)
+					h.Set("Mime-Version", "1.0")
+
+					return *h
+				}(),
+				Parts:    []Part{},
+				boundary: "+Go+User+Data+Boundary==",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := NewUserData()
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func TestNewUserDataWithBoundary(t *testing.T) {
+	type args struct {
+		boundary string
+	}
+
+	tests := []struct {
+		name     string
+		args     args
+		expected *UserData
+	}{
+		{
+			name: "positive case: quoted",
+			args: args{
+				boundary: "+Go+User+Data+Boundary==",
+			},
+			expected: &UserData{
+				Header: func() Header {
+					h := NewHeader()
+
+					h.Set("Content-Type", "multipart/mixed; boundary=\"+Go+User+Data+Boundary==\"")
+					h.Set("Mime-Version", "1.0")
+
+					return *h
+				}(),
+				Parts:    []Part{},
+				boundary: "+Go+User+Data+Boundary==",
+			},
+		},
+		{
+			name: "positive case: non quoted",
+			args: args{
+				boundary: "+Go+User+Data+Boundary++",
+			},
+			expected: &UserData{
+				Header: func() Header {
+					h := NewHeader()
+
+					h.Set("Content-Type", "multipart/mixed; boundary=+Go+User+Data+Boundary++")
+					h.Set("Mime-Version", "1.0")
+
+					return *h
+				}(),
+				Parts:    []Part{},
+				boundary: "+Go+User+Data+Boundary++",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := NewUserDataWithBoundary(tt.args.boundary)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func TestUserData_AddPart(t *testing.T) {
+	type args struct {
+		mediaType string
+		body      []byte
+	}
+
+	tests := []struct {
+		name     string
+		userData UserData
+		args     []args
+		expected UserData
+	}{
+		{
+			name:     "positive case: ascii only",
+			userData: *NewUserData(),
+			args: []args{
+				{
+					mediaType: "text/cloud-config",
+					body:      []byte("#cloud-config\n" + "timezone: America/Virgin"),
+				},
+				{
+					mediaType: "text/x-shellscript",
+					body:      []byte("#!/bin/bash\n" + "echo 'Hello World'"),
+				},
+			},
+			expected: UserData{
+				Header: func() Header {
+					h := NewHeader()
+
+					h.Set("Content-Type", "multipart/mixed; boundary=\"+Go+User+Data+Boundary==\"")
+					h.Set("Mime-Version", "1.0")
+
+					return *h
+				}(),
+				Parts: []Part{
+					{
+						Header: func() Header {
+							h := NewHeader()
+
+							h.Set("Content-Transfer-Encoding", "7bit")
+							h.Set("Content-Type", "text/cloud-config; charset=us-ascii")
+
+							return *h
+						}(),
+						Body: []byte("#cloud-config\n" + "timezone: America/Virgin"),
+					},
+					{
+						Header: func() Header {
+							h := NewHeader()
+
+							h.Set("Content-Transfer-Encoding", "7bit")
+							h.Set("Content-Type", "text/x-shellscript; charset=us-ascii")
+
+							return *h
+						}(),
+						Body: []byte("#!/bin/bash\n" + "echo 'Hello World'"),
+					},
+				},
+				boundary: "+Go+User+Data+Boundary==",
+			},
+		},
+		{
+			name:     "positive case: include utf-8",
+			userData: *NewUserData(),
+			args: []args{
+				{
+					mediaType: "text/cloud-config",
+					body:      []byte("#cloud-config\n" + "timezone: Asia/Tokyo"),
+				},
+				{
+					mediaType: "text/x-shellscript",
+					body:      []byte("#!/bin/bash\n" + "echo 'こんにちは世界'"),
+				},
+			},
+			expected: UserData{
+				Header: func() Header {
+					h := NewHeader()
+
+					h.Set("Content-Type", "multipart/mixed; boundary=\"+Go+User+Data+Boundary==\"")
+					h.Set("Mime-Version", "1.0")
+
+					return *h
+				}(),
+				Parts: []Part{
+					{
+						Header: func() Header {
+							h := NewHeader()
+
+							h.Set("Content-Transfer-Encoding", "7bit")
+							h.Set("Content-Type", "text/cloud-config; charset=us-ascii")
+
+							return *h
+						}(),
+						Body: []byte("#cloud-config\n" + "timezone: Asia/Tokyo"),
+					},
+					{
+						Header: func() Header {
+							h := NewHeader()
+
+							h.Set("Content-Transfer-Encoding", "base64")
+							h.Set("Content-Type", "text/x-shellscript; charset=utf-8")
+
+							return *h
+						}(),
+						Body: []byte(base64.StdEncoding.EncodeToString([]byte("#!/bin/bash\n" + "echo 'こんにちは世界'"))),
+					},
+				},
+				boundary: "+Go+User+Data+Boundary==",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, args := range tt.args {
+				tt.userData.AddPart(args.mediaType, args.body)
+			}
+			assert.Equal(t, tt.expected, tt.userData)
+		})
+	}
+}
+
+func TestUserData_Render(t *testing.T) {
+	tests := []struct {
+		name     string
+		userData UserData
+		expected []byte
+		err      error
+	}{
+		{
+			name: "positive case: ascii only",
+			userData: func() UserData {
+				d := NewUserData()
+
+				d.AddPart("text/cloud-config", []byte("#cloud-config\n"+"timezone: America/Virgin"))
+				d.AddPart("text/x-shellscript", []byte("#!/bin/bash\n"+"echo 'Hello World'"))
+
+				return *d
+			}(),
+			expected: []byte(
+				"Content-Type: multipart/mixed; boundary=\"+Go+User+Data+Boundary==\"\r\n" +
+					"Mime-Version: 1.0\r\n" +
+					"\r\n" +
+					"--+Go+User+Data+Boundary==\r\n" +
+					"Content-Transfer-Encoding: 7bit\r\n" +
+					"Content-Type: text/cloud-config; charset=us-ascii\r\n" +
+					"\r\n" +
+					"#cloud-config\n" + "timezone: America/Virgin" +
+					"\r\n" +
+					"\r\n" +
+					"--+Go+User+Data+Boundary==\r\n" +
+					"Content-Transfer-Encoding: 7bit\r\n" +
+					"Content-Type: text/x-shellscript; charset=us-ascii\r\n" +
+					"\r\n" +
+					"#!/bin/bash\n" + "echo 'Hello World'" +
+					"\r\n" +
+					"\r\n" +
+					"--+Go+User+Data+Boundary==--\r\n",
+			),
+		},
+		{
+			name: "positive case: include utf-8",
+			userData: func() UserData {
+				d := NewUserData()
+
+				d.AddPart("text/cloud-config", []byte("#cloud-config\n"+"timezone: Asia/Tokyo"))
+				d.AddPart("text/x-shellscript", []byte("#!/bin/bash\n"+"echo 'こんにちは世界'"))
+
+				return *d
+			}(),
+			expected: []byte(
+				"Content-Type: multipart/mixed; boundary=\"+Go+User+Data+Boundary==\"\r\n" +
+					"Mime-Version: 1.0\r\n" +
+					"\r\n" +
+					"--+Go+User+Data+Boundary==\r\n" +
+					"Content-Transfer-Encoding: 7bit\r\n" +
+					"Content-Type: text/cloud-config; charset=us-ascii\r\n" +
+					"\r\n" +
+					"#cloud-config\n" + "timezone: Asia/Tokyo" +
+					"\r\n" +
+					"\r\n" +
+					"--+Go+User+Data+Boundary==\r\n" +
+					"Content-Transfer-Encoding: base64\r\n" +
+					"Content-Type: text/x-shellscript; charset=utf-8\r\n" +
+					"\r\n" +
+					base64.StdEncoding.EncodeToString([]byte("#!/bin/bash\n"+"echo 'こんにちは世界'")) +
+					"\r\n" +
+					"\r\n" +
+					"--+Go+User+Data+Boundary==--\r\n",
+			),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := tt.userData.Render()
+
+			if tt.err == nil {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expected, actual)
+			} else {
+				assert.Error(t, err)
+				assert.Equal(t, tt.err, err)
+			}
+		})
+	}
+}
